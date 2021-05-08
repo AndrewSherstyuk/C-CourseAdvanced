b6859f8c7ea1d4f0eafc45de6b8a616cfba7d8ab
AndrewSherstyuk <andrey.sherstyuk@dataart.com>
2021-05-08 15:25:03 +0300

    Commit after resplacements

--- Lesson3_Homework/Task4/MainWindow.xaml.cs
+++ Lesson3_Homework/Task4/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Task4
 {
@@ -23,6 +24,39 @@ namespace Task4
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //private void Window_Loaded(Object sender, RoutedEventHandler eventHandler)
+        //{
+        //    DispatcherTimer dt = new DispatcherTimer();
+        //    dt.Interval = TimeSpan.FromSeconds(1);
+        //    dt.Tick += dtTicker;
+        //    dt.Start();
+        //}
+
+        
+        //private void StopButtonClick(Object sender, RoutedEventArgs e)
+        //{
+
+        //}
+
+        //private void ResetButtonClick(Object sender, RoutedEventArgs e)
+        //{
+
+        //}
+
+        private int increment = 0;
+
+        private void dtTicker(object sender, EventArgs e)
+        {
+            increment++;
+            TimerLabel.Content = increment.ToString();
+        }
+
+        private void StartButton_Click_1(object sender, EventArgs e)
+        {
+            increment++;
+            TimerLabel.Content = increment.ToString();
         }
     }
 }
